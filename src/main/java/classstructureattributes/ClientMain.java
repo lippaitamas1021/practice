5c104463927d81f5b849a545423ffdbe3765bf9c
lippaitamas1021 <lippaitamas1021@gmail.com>
2020-11-27 10:51:16 +0100

    Client commit

---
+++ src/main/java/classstructureattributes/ClientMain.java
@@ -0,0 +1,25 @@
+package classstructureattributes;
+
+import java.util.Scanner;
+
+public class ClientMain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Client client = new Client();
+
+        System.out.println("Kérem, adja meg a nevét:");
+        client.name = scanner.nextLine();
+
+        System.out.println("Kérem, adja meg a születési évét:");
+        client.year = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Kérem, adja meg a címét:");
+        client.address = scanner.nextLine();
+
+        System.out.println(
+                "Regisztrációs adatok: Név: " + client.name + ", Születési év: " + client.year + ", Cím: " + client.address
+        );
+    }
+
+}
