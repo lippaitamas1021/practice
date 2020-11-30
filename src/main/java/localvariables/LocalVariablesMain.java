ea94a00acf1448e7e815d284e4411cbfc8ed3833
lippaitamas1021 <lippaitamas1021@gmail.com>
2020-11-30 10:03:34 +0100

    LocalVariables commit

---
+++ src/main/java/localvariables/LocalVariablesMain.java
@@ -0,0 +1,18 @@
+package localvariables;
+
+public class LocalVariablesMain {
+    public static void main(String[] args) {
+        boolean b = false;
+        System.out.println(b);
+
+        int a = 2;
+        int i = 3, j = 4;
+        int k = i;
+        String s = "Hello, World!";
+        String t = s;
+        System.out.println("" + a + i + j + k + s + t);
+
+        }
+
+    }
+
