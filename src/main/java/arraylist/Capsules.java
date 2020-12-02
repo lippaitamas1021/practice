46c814d4dd189fe1641532a1c3a588fcb4fd5c83
lippaitamas1021 <lippaitamas1021@gmail.com>
2020-12-02 16:02:27 +0100

    Capsules commit #2

--- src/main/java/arraylist/Capsules.java
+++ src/main/java/arraylist/Capsules.java
@@ -9,23 +9,28 @@ public class Capsules {
     private List<String> colors = new ArrayList<>();
 
     public void addLast(String color) {
+
         colors.add(color);
     }
 
     public void addFirst(String color) {
+
         colors.add(color);
     }
 
     public void removeFirst() {
+
         colors.remove(0);
     }
 
     public void removeLast() {
+
         colors.remove(colors.size() - 1);
     }
 
     public List<String> getColors() {
-       return colors;
+
+        return colors;
     }
 
     public static void main(String[] args) {
