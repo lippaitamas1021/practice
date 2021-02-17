6343e3fac28e4d6380353fe55e2e147b9ab7637e
lippaitamas1021 <lippaitamas1021@gmail.com>
2021-02-17 16:42:33 +0100

    week05d02 commit

---
+++ src/main/java/array/week05d02/ChangeLetter.java
@@ -0,0 +1,25 @@
+package array.week05d02;
+
+public class ChangeLetter {
+
+    private static final String VOWELS = "aeiou";
+    private static final String REPLACE = "*";
+
+    public String changeVowels(String word) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) {
+            String s = word.substring(i, i+1);
+            if (VOWELS.contains(s)) {
+                sb.append(REPLACE);
+            } else {
+                sb.append(s);
+            }
+            }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        ChangeLetter cl = new ChangeLetter();
+        System.out.println(cl.changeVowels("almafa"));
+    }
+}
