ee4fbddc07411fd651c28477e626142b1793e6f9
lippaitamas1021 <lippaitamas1021@gmail.com>
2021-02-17 14:32:36 +0100

    week02d02 commit

---
+++ src/main/java/week02d02/Phone.java
@@ -0,0 +1,41 @@
+package week02d02;
+
+public class Phone {
+
+    private String type;
+    private int mem;
+
+    public Phone(String type, int mem) {
+        this.type = type;
+        this.mem = mem;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getMem() {
+        return mem;
+    }
+
+    public void setMem(int mem) {
+        this.mem = mem;
+    }
+
+    @Override
+    public String toString() {
+        return "Phone{" +
+                "type='" + type + '\'' +
+                ", mem=" + mem + " Gb" +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Phone phone = new Phone("iPhone", 32);
+        System.out.println(phone);
+    }
+}
