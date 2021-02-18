153b308151eb355268663d6a7be876139a72874e
lippaitamas1021 <lippaitamas1021@gmail.com>
2021-02-18 16:10:00 +0100

    week08d04 commit

---
+++ src/main/java/week08d04/Trainer.java
@@ -0,0 +1,29 @@
+package week08d04;
+
+public class Trainer {
+
+    private CanMark canMark;
+
+    public Trainer(CanMark canMark) {
+        this.canMark = canMark;
+    }
+
+    public int giveMark() {
+        return canMark.giveMark();
+    }
+
+    public void setMood(CanMark canMark) {
+        this.canMark = canMark;
+    }
+
+    public static void main(String[] args) {
+        Trainer goodTrainer = new Trainer(new GoodMood());
+        Trainer badTrainer = new Trainer(new BadMood());
+
+        System.out.println(goodTrainer.giveMark());
+        System.out.println(badTrainer.giveMark());
+
+        goodTrainer.setMood(new BadMood());
+        System.out.println(goodTrainer.giveMark());
+    }
+}
