a02d131e26dd636f936237d2a5918343e146d527
lippaitamas1021 <lippaitamas1021@gmail.com>
2021-02-19 10:57:14 +0100

    exam02 commit #2

---
+++ src/main/java/exam02/ArraySelector.java
@@ -0,0 +1,23 @@
+package exam02;
+
+public class ArraySelector {
+
+    public String selectEvens(int[] numbers) {
+
+        StringBuilder sb = new StringBuilder("");
+
+        if (0 < numbers.length) {
+            sb.append("[");
+        } else {
+            return "";
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (i % 2 == 0) {
+                sb.append(numbers[i]).append(", ");
+            }
+        }
+        sb.delete(sb.length() - 2, sb.length());
+        return sb.append("]").toString();
+    }
+}
---
+++ src/main/java/exam02/cv/Cv.java
@@ -0,0 +1,39 @@
+package exam02.cv;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cv {
+
+    private String name;
+    private List<Skill> skills = new ArrayList<>();
+
+    public Cv(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Skill> getSkills() {
+        return skills;
+    }
+
+    public void addSkills(String... skills) {
+        for (String s : skills) {
+        String name = s.substring(0, s.indexOf("(")).trim();
+        int level = Integer.parseInt(s.substring(s.indexOf("(") + 1, s.indexOf(")")));
+        this.skills.add(new Skill(name, level));
+        }
+    }
+
+    public int findSkillLevelByName(String name) {
+        for (Skill s : skills) {
+            if (s.getName().equals(name)) {
+                return s.getLevel();
+            }
+        }
+        throw new IllegalArgumentException("Wrong skill name");
+    }
+}
