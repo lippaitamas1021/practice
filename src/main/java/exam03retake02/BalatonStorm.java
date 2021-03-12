b3838f3fe982cb33d797a16110ab2d5bf66b669c
lippaitamas1021 <lippaitamas1021@gmail.com>
2021-03-12 18:00:51 +0100

    exam03retake02 BalatonStorm commit

---
+++ src/main/java/exam03retake02/BalatonStorm.java
@@ -0,0 +1,35 @@
+package exam03retake02;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.text.Collator;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class BalatonStorm {
+
+    public List<String> getStationsInStorm(BufferedReader reader) throws IOException {
+        List<String> stations = new ArrayList<>();
+        String line;
+        String station = null;
+        while ((line = reader.readLine()) != null) {
+            if (line.contains("\"allomas\"")) {
+                station = extractStationName(line);
+            }
+            if (isStorm(line)) {
+                stations.add(station);
+            }
+        }
+        stations.sort(Collator.getInstance(new Locale("hu", "HU")));
+        return stations;
+    }
+
+    private boolean isStorm(String line) {
+        return line.contains("\"level\": 3");
+    }
+
+    private String extractStationName(String line) {
+        return line.substring(line.lastIndexOf(": \"") + 3, line.lastIndexOf("\""));
+    }
+}
